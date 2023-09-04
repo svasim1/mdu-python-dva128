eaebc22263ec9ac4a151f42cc2faec583b6094a7
svasim <svante.ericsson06@gmail.com>
2023-09-04 08:33:59 +0200

    Updated calculator.py
    Co-authored-by: Filip Galin <Abb-FilGal@users.noreply.github.com>
    Co-authored-by: lucasAxberg <lucasAxberg@users.noreply.github.com>

--- case/case1/calculator.py
+++ case/case1/calculator.py
@@ -1,3 +1,51 @@
 import math
 
-print('******************************************' + '\n' + 'CALCULATOR' + '\n' + '------------------------------------------')
+operators = {'add': '+', 'sub': '-', 'mul': '*', 'div': '/',}
+
+def add(a,b):
+    return f"RESULT: {a} + {b} = {a+b}";
+def sub(a,b):
+    return a - b;
+def mul(a,b):
+    return a * b;
+def div(a,b):
+    return a / b;
+
+def selector(mType, a, b):
+    if mType == "add":
+        return add(a, b)
+    elif mType == "sub":
+        return sub(a, b)
+    elif mType == "mul":
+        return mul(a, b)
+    elif mType == "div":
+        return div(a, b)
+
+close = False
+while not close:
+    print('******************************************' + '\n' + '   Mathlete Calculator' + '\n' + '------------------------------------------' + "\n" + "add | Add two numbers" + "\n" + "sub | Subtract two number" + "\n" + "mul | Multiply two numbers" + "\n" + "div | Divide two numbers" + "\n" + '------------------------------------------')
+    mathType = input('Selection > ')
+    if mathType == "add" or mathType == "sub" or mathType == "mul" or mathType == "div":
+        print('------------------------------------------' + "\n" + "Calculating 'c' for expression:" + "\n\n" + "a " + operators[mathType] + " b = c" + "\n\n" + "Please, enter values for 'a' and 'b'." + "\n")
+        try:
+            primary = float(input('a = '))
+            secondary = float(input('b = '))
+            if secondary == 0 and mathType == 'div':
+                print('Undefined')
+                input("Press ENTER to continue... ")
+                continue
+        except ValueError:
+            print("Please enter a valid number")
+            input("Press ENTER to continue... ")
+            continue
+        print(selector(mathType, primary, secondary))
+        input("Press ENTER to continue... ")
+        continue
+    elif mathType == "exit":
+        close = True
+    else:
+        print("Please enter a valid mode")
+        input("Press ENTER to continue... ")
+        continue
+
+   
