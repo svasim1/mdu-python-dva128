e728cee4b5a6aef90cfa1a676407d1076bfe5f7f
svasim1 <svante.ericsson06@gmail.com>
2023-09-14 21:41:41 +0200

    Commented

--- weekly/week3/three_day_planner.py
+++ weekly/week3/three_day_planner.py
@@ -1,9 +1,11 @@
 import os
 
+# Variable declaration
 goal_today = "Run 5km"
 goal_tomorrow = "Swim 1km"
 goal_later = "Bike 10km"
 
+# Print instructions
 def instructions():
     header = f""".: THREE DAY PLANNER :.
 -----------------------
@@ -20,6 +22,7 @@ e | Exit program
 -----------------------"""
     print(header)
 
+# Function to cycle between goals for each day
 def cycle():
     global goal_today
     global goal_tomorrow
@@ -28,6 +31,7 @@ def cycle():
     goal_tomorrow = goal_later
     goal_later = ""
 
+# Function to change goals on selected day
 def change_goal():
     global goal_today
     global goal_tomorrow
@@ -44,6 +48,7 @@ def change_goal():
         goal_tomorrow = input("Goal for tomorrow > ")
     elif index == "2":
         goal_later = input("Goal for later > ")
+    # Error handling
     else:
         print("""-----------------------
 ERROR: Bad day
@@ -51,6 +56,7 @@ INFO:  Please enter a valid integer between 0 and 2.
 -----------------------""")
         input("Press enter to continue...")
 
+# Function to check input
 def check_operator(operator):
         if operator.lower() == "n":
             cycle()
@@ -59,11 +65,13 @@ def check_operator(operator):
         elif operator.lower() == "e" or operator.lower() == "exit":
             exit()
         else:
+            # Error handling
             print(f"""-----------------------
 ERROR: Unknown operator ({operator})
 -----------------------""")
             input("Press enter to continue...")
             
+# Main loop
 os.system("cls")
 while True:
     os.system("cls")
