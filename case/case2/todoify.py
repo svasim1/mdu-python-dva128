264c121e8b61bb8cd3c82a22b16ba0208d4bd89e
svasim <svante.ericsson06@gmail.com>
2023-09-20 14:56:22 +0200

    Co-authored-by: Filip Galin <Abb-FilGal@users.noreply.github.com> Co-authored-by: lucasAxberg <lucasAxberg@users.noreply.github.com>

--- case/case2/todoify.py
+++ case/case2/todoify.py
@@ -1,6 +1,11 @@
+# Import necessary modules
 import os
+import json
 
-todos = ["[X] Plugga", "[ ] Städa", "[ ] Andas", "[ ] Kalle Kula"]
+# Initialize an empty list to store todos
+todos = []
+
+# Function to display instructions for using the application
 def instructions():
     header = """
     *****************************
@@ -16,54 +21,79 @@ def instructions():
     -----------------------------
     """
     print(header)
-       
+
+# Function to save the todos to a file in JSON format
 def save_todos():
     file = "todos"
-    with open(f"{file}.txt", "a", encoding="utf-8") as save_file:
-        for todo in todos:
-            save_file.write(f"{todo}\n")
+    with open(f"{file}.txt", "w", encoding="utf-8") as save_file:
+        save_file.write(json.dumps(todos))
 
+# Function to load todos from a file
 def load_todos(file):
     try:
         with open(f"{file}.txt", "r") as load_file:
-            for line in load_file:
-                todos.append(line.replace("\n", ""))
+            global todos
+            todos = json.loads(load_file.readline())
     except FileExistsError:
         print("ERROR: FILE NOT FOUND")
-    
-def list_todo():    
+
+# Function to list all the todos
+def list_todo():
     print("-----------------------------")
     for todo in todos:
         print(todo)
     print("-----------------------------")
-    
+
+# Function to add a new todo
 def add_todo():
     print("-----------------------------")
-    todo = input("Todo description: ")          
+    todo = input("Todo description: ")
     todos.append(f"[ ] {todo}")
-    
+
+# Function to check/uncheck a todo as done
 def check_todo():
+    
     print("-----------------------------")
     for i in range(len(todos)):
         print(f"{i} | {todos[i]}")
     print("-----------------------------")
-    index = int(input("Todo index > "))
-    if todos[index][1] == ' ':
-        new_line = todos[index].replace(' ','X',1)
-        todos[index] = new_line
-    else:
-        new_line = todos[index].replace('X',' ',1)
-        todos[index] = new_line
+    
+    try:
+        index = int(input("Todo index > "))
+    except ValueError:
+        print(f'ERROR: Please input a valid number')
+        return
+    
+    try:
+        if todos[index][1] == ' ':
+            new_line = todos[index].replace(' ', 'X', 1)
+            todos[index] = new_line
+        else:
+            new_line = todos[index].replace('X', ' ', 1)
+            todos[index] = new_line
+    except IndexError:
+        print(f"ERROR: Index ({index}) out of range")
 
+# Function to delete a todo
 def delete_todo():
+    
     print("-----------------------------")
     for i in range(len(todos)):
-        
         print(f"{i} | {todos[i]}")
     print("-----------------------------")
-    index = input("Todo index > ")
-    del todos[int(index)]
+    
+    try:
+        index = int(input("Todo index > "))
+    except ValueError:
+        print(f'ERROR: Please input a valid number')
+        return
+    
+    try:
+        del todos[index]
+    except IndexError:
+        print(f'ERROR: Index ({index}) out of range')
 
+# Check the input and choose the right operator
 def check_operator(input):
     if input.lower() == 'list':
         list_todo()
@@ -79,13 +109,16 @@ def check_operator(input):
         load_todos("todos")
     elif input.lower() == 'exit':
         exit()
-    
+    else:
+        print(f'ERROR: Invalid operator ({operator})')
+
+# Define a variable to control the loop
 close = False
+
+# Main loop for user interaction
 while not close:
-    os.system('cls')
-    instructions()
+    os.system('cls')  # Clears the screen
+    instructions()  
     operator = input('Selection > ')
     check_operator(operator)
     input("PRESS ENTER TO CONTINUE...")
-
-         
