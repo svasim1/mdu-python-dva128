79825e161cd549b280b88e9ed6e99b6590723ae5
svasim <svante.ericsson06@gmail.com>
2023-10-18 10:10:14 +0200

    Updated case4 - still not finalized
    Co-authored-by: lucasAxberg <lucasAxberg@users.noreply.github.com>
    Co-authored-by: Filip Galin <Abb-FilGal@users.noreply.github.com>

--- case/case4/football_frenzy.py
+++ case/case4/football_frenzy.py
@@ -10,26 +10,29 @@ def request_handler(api_url):
     return request
 
 def save_season_data(api_url):
-    print(api_url)
     data = request_handler(api_url)
-    print(data)
     gamedays = data["gamedays"]
-    print(gamedays)
     teams = data["teams"]
     scoretable = {}
     for team in teams:
         scoretable[team] = [0, 0, 0, 0]
     for date in gamedays:
         change_results(scoretable, request_handler(api_url + "/" + date))
-    print(scoretable)
+    return scoretable
 
 def change_results(dic, data):
+    # Takes out the list of games for the day that wass passed in
     matches = data["games"]
-    for key in matches:
-        home_score = matches[key]["score"]["home"]["goals"]
-        home_team = matches[key]["score"]["home"]["team"]
-        away_score = matches[key]["score"]["away"]["goals"]
-        away_team = matches[key]["score"]["home"]["team"]
+    
+    # Loops through each match for the day
+    for match in matches:
+        # Saves the name of the home team, and their score
+        home_score = match["score"]["home"]["goals"]
+        home_team = match["score"]["home"]["team"]
+        
+        # Saves the name of the away team, and their score
+        away_score = match["score"]["away"]["goals"]
+        away_team = match["score"]["away"]["team"]
 
 
         # TODO: fixa pa battre satt
@@ -55,19 +58,31 @@ def change_results(dic, data):
             dic[away_team][1] += 1
             
             # Increase P by 1
-            dic[home_team][2] += 1
-            dic[away_team][2] += 1
+            dic[home_team][3] += 1
+            dic[away_team][3] += 1
 
             
 def list_seasons():
     seasons = request_handler(base_url)["seasons"]
     print("-"*40)
     for key in seasons:
-        print(f"| {seasons[key]}")
+        print(f"| {seasons[key]}") 
     
 def view_season():
     selected_year = input("| Year > ")
-    save_season_data(base_url + "/" + selected_year)
+    scoretable = save_season_data(base_url + "/" + selected_year)
+    
+    print("|" + " "*39)
+    print(f"| {'Team'.ljust(26)}{'W'.ljust(7)}{'D'.ljust(7)}{'L'.ljust(7)}{'P'.ljust(7)}")
+    print("| ".ljust(25, '-')  + "  ---    ---    ---    ---")
+    
+    for team_name in scoretable:
+        team_data = scoretable[team_name]
+        team_data = [str(x) for x in team_data]
+
+        
+        
+        print("|", team_name.ljust(25), team_data[0].ljust(6), team_data[1].ljust(6), team_data[2].ljust(6), team_data[3].ljust(6))
 
 
 operations = {
@@ -75,16 +90,12 @@ operations = {
     "view": view_season
 }
 
-score = {
-    
-}
-
-view_season()
-#request_handler(base_url + "/2018")
-while not True:
+while True:
     os.system("cls" if os.name == "nt" else "clear")
     print("-"*40)
-    operator = input("Selection > ")
+    operator = input("| Selection > ")
     operations[operator]()
     input("Press enter to continue...")
+    
+
     
